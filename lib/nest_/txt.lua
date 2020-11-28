347d4a2a4756bb8d527ea18c21730839a46badea
andrew <andrewcshike@gmail.com>
2020-11-29 04:49:37 +0700

    start txtline

--- lib/nest_/txt.lua
+++ lib/nest_/txt.lua
@@ -20,67 +20,71 @@ _txt.affordance = _screen.affordance:new {
 
 _txt.affordance.output.txt = function(s) end
 
---[[
+local function txtpoint(txt, a)
+    -- x, y, align font, size, lvl, border, fill, padding, font_headroom, font_leftroom
 
-txt0d
-txt1d
-txt2d
+    local d = { 
+        x = { nil, nil }, 
+        y = { nil, nil },
+        align = { 'left', 'top' } 
+    }
 
-]]
+    for _,k in ipairs { 'x', 'y', 'align' } do 
+        if type(a[k]) == 'table' then d[k] = a[k] 
+        else d[k][1] = a[k] end    
+    end
 
-local function txtbox(txt, x, y, x2, y2, xalign, yalign, font, size, lvl, border, fill, padding, font_headroom, font_leftroom)
-    font_headroom = font_headroom or 3/8 
-    font_leftroom = font_leftroom or 1/16
+    screen.font_face(a.font)
+    screen.font_size(a.size)
 
-    screen.font_face(font)
-    screen.font_size(size)
-
-    local fixed = (x2 ~= nil) and (y2 ~= nil)
+    local fixed = (d.x[2] ~= nil) and (d.y[2] ~= nil)
     local width = screen.text_extents(txt)
-    local height = size * (1 - font_headroom)
+    local height = a.size * (1 - font_headroom)
     local w, h, bx, by, tx, ty, tmode
 
     if fixed then
-        w = x2 - x - 1
-        h = y2 - y - 1
+        w = d.x[2] - d.x[1] - 1
+        h = d.y[2] - d.y[1] - 1
 
-        bx = x
-        by = y
+        bx = d.x[1]
+        by = d.y[1]
         tx = bx + w/2 - 1
         ty = by + (h + height)/2 - 1
         tmode = 'center'
     else
-        local px = (type(padding) == 'table' and padding[1] or padding) * 2
-        local py = (type(padding) == 'table' and padding[2] or padding) * 2
+        local px = (type(a.padding) == 'table' and a.padding[1] or a.padding or 0) * 2
+        local py = (type(a.padding) == 'table' and a.padding[2] or a.padding or 0) * 2
 
         w = width + px - 1
         h = height + py - 1
 
+        local xalign = a.align[1]
+        local yalign = a.align[2]
         local bxalign = (xalign == 'center') and (((width + px) / 2) + 1) or (xalign == 'right') and (width + px) or 0
         local byalign = (yalign == 'center') and h/2 - 1 or (yalign == 'bottom') and h or 0
         local txalign = (xalign == 'center') and 0 or (xalign == 'right') and -1 or 1
         local tyalign = (yalign == 'center') and (height/2) or (yalign == 'bottom') and - (py/2) or height + (py/2) - 1
 
-        bx = x - bxalign
-        by = y - byalign
-        tx = x - 1 + ((px / 2) * txalign) - (font == 1 and (size * font_leftroom) or 0)
-        ty = y + tyalign
+        bx = d.x[1] - bxalign
+        by = d.y[1] - byalign
+        tx = d.x[1] - 1 + ((px / 2) * txalign) - (a.font == 1 and (a.size * font_leftroom) or 0)
+        ty = d.y[1] + tyalign
         tmode = xalign
     end
 
-    if fill > 0 then
-        screen.level(fill)
+    if a.fill > 0 then
+        screen.level(a.fill)
         screen.rect(bx - 1, by - 1, w + 1, h + 1)
         screen.fill()
     end
 
-    if border > 0 then
-        screen.level(border)
+    if a.border > 0 then
+        screen.level(a.border)
         screen.rect(bx, by, w, h)
         screen.stroke()
     end
 
-    screen.level(lvl)
+    screen.level(a.lvl)
     screen.move(tx, ty)
 
     if tmode == 'right' then
@@ -94,20 +98,83 @@ local function txtbox(txt, x, y, x2, y2, xalign, yalign, font, size, lvl, border
     return w, h
 end
 
-function txtlist(txt, x, y, x2, y2, flow, wrap, margin, cellsize)
+function txtline(txt, a)
+    --x, y, align, flow, wrap, margin, cellsize
+
+    --[[
+        start: x = n
+        justify: x = { n, n },
+        manual: x = { { n, n }, { n }, ... }
+    ]]
+
+    local flow = a.flow
+    local start, justify, manual = 1, 2, 3
+    local mode = start
+    local iax = {}
+    local ax = { 'x', 'y' }
+
+    for i,k in ipairs(ax) do
+        if type(a[k]) == 'table' then
+            if type(a[k][1]) ~= 'table' then
+                if mode == start then
+                    mode = justify
+                    flow = k
+                    iax[k] = a[k][1]
+                end
+            else
+                if mode == manual then
+                    flow = nil
+                else
+                    mode = manual
+                    flow = k
+                end
+            end
+        else
+            iax[k] = a[k]
+        end
+    end
+
+    local function setetc(pa, i) 
+        for j,k in ipairs { 'font', 'size', 'lvl', 'border', 'fill', 'font_headroom', 'font_leftroom' } do 
+            local w = a[k]
+            pa[k] = (type(w) == 'table') and w[i] or w
+        end
+
+        pa.padding = a.padding
+    end
+
+    local function putpoint(v, pa, xy)
+        for j,k in ipairs(ax) do
+            if a.cellsize then
+                pa[k] = { xy[k], iax[k] + cellsize[j] }
+            else 
+                pa[k] = { xy[k] }
+            end
+        end
+
+        return txtpoint(v, pa)
+    end
+   
+    if mode == start then
+        for i,v in ipairs(txt) do 
+            local pa = {}
+            local dim = {} 
+
+            setetc(pa, i)
+            dim.x, dim.y = putpoint(v, pa, iax)
+
+            ---------------------------------- scoot !
+        end
+    end
+
+    --return w, h
 end
 
 _txt.affordance.output.txtdraw = function(s, txt) 
     if type(txt) == 'table' then
-                
+        txtline(txt, s)                
     else
-        local c = { x = { nil, nil }, y = { nil, nil }, align = { 'left', 'top' } }
-        for _,k in ipairs { 'x', 'y', 'align' } do 
-            if type(s[k]) == 'table' then c[k] = s[k] 
-            else c[k][1] = s[k] end    
-        end
-            
-        txtbox(txt, c.x[1], c.y[1], c.x[2], c.y[2], c.align[1], c.align[2], s.font, s.size, s.lvl, s.border, s.fill, s.padding, s.font_headroom, s.font_leftroom)
+        txtpoint(txt, s)   
     end
 end
 
