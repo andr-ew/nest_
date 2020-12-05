18ab8ef36f258e78f795b3d1b7c99c59ebc0a767
andrew <andrewcshike@gmail.com>
2020-12-05 12:28:41 +0700

    notes

--- test.lua
+++ test.lua
@@ -9,35 +9,23 @@ include 'lib/nest_/txt'
 
 tab = require 'tabutil'
 
---test manual & cellsize
+--test cellsize
 --maybe refactor cellsize into txtpoint instead of txtline, it probably bugs out rn
+--also maybe add cellsize 'auto', because I'm crazy & we've gone this deep already
 
 n = nest_ {
     _txt.label {
-        --y = { 4, 60 },
         y = 4,
-        --x = 6,
-        --x = { 4, 124 },
         x = {
             { { 4, 24 }, { 28, 48 }, { 52, 72 }, { 90, 126 } },
             { { 4, 24 }, { 28, 48 }, { 52, 72 }, { 80, 126 } },
             { { 4, 24 }, { 28, 48 }, { 52, 72 }, { 80, 126 } }
         },
-        --[[
-        y = { 
-            { { 2, 24 }, { 2, 24 }, { 2, 24 }, { 2, 24 },}, 
-            { { 32, 44 }, { 32, 44 }, { 32, 44 }, { 32, 44 }, },
-            { { 40, 62 }, { 40, 62 }, { 40, 62 }, { 40, 62 }, }
-        },
-        --]]
-        --value = { 'foo', 'bar', 'ding', 'bat' },
-        ---[[
         value = { 
             { 'foo', 'bar', 'ding', 'bat' },
             { 'bar', 'foo', 'bat', 'ding' },
             { 'bat', 'ding', 'bar', 'foo' }
         },
-        --]]
         border = 15,
         padding = 3,
         margin = 3,
