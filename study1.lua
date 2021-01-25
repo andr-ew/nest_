4fbadafbc4ec9f538838681f3ebaec087f2d089e
andrew <andrewcshike@gmail.com>
2021-01-26 02:11:53 +0700

    study1

--- study1.lua
+++ study1.lua
@@ -3,23 +3,35 @@ include 'lib/nest_/norns'
 
 dave = nest_ {
     tommy = _affordance {
-        value = 4,
-        action = function(self, value)
-            print("my value is " .. value)
-        end
-    },
-    walter = _affordance {
         value = 5,
         action = function(self, value)
+            print("my name is " .. self.key)
             print("my value is " .. value)
         end
     },
     elanore = _affordance {
         value = 6,
         action = function(self, value)
+            print("my name is " .. self.key)
             print("my value is " .. value)
             print("tommy's value is " .. self.p.tommy.value)
         end
+    },
+    things = nest_ {
+        _affordance {
+            value = 7,
+            action = function(self, value)
+                print("I'm thing number " .. self.key)
+                print("my value is " .. value)
+            end
+        },
+        _affordance {
+            value = 8,
+            action = function(self, value)
+                print("I'm thing number " .. self.key)
+                print("my value is " .. value)
+            end
+        }
     }
 }
 
