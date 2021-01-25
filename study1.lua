22b85383fe3630cb205b56fc7ad81d7757be6396
andrew <andrewcshike@gmail.com>
2021-01-26 01:40:48 +0700

    study1

--- study1.lua
+++ study1.lua
@@ -1,4 +1,28 @@
 include 'lib/nest_/core'
 include 'lib/nest_/norns'
 
+dave = nest_ {
+    tommy = _affordance {
+        value = 4,
+        action = function(self, value)
+            print("my value is " .. value)
+        end
+    },
+    walter = _affordance {
+        value = 5,
+        action = function(self, value)
+            print("my value is " .. value)
+        end
+    },
+    elanore = _affordance {
+        value = 6,
+        action = function(self, value)
+            print("my value is " .. value)
+            print("tommy's value is " .. self.p.tommy.value)
+        end
+    }
+}
 
+function init()
+    dave:init()
+end
