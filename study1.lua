e3a202658facd4d3172de2e109b953d4d398e119
andrew <andrewcshike@gmail.com>
2021-02-02 12:13:19 +0700

    numberkeys

--- study1.lua
+++ study1.lua
@@ -2,10 +2,10 @@
 -- nested affordances
 --
 --    1-5
--- 1: strum a
--- 2: strum b
--- 3: strum c
--- 4: strum d
+-- 1: strum 1
+-- 2: strum 2
+-- 3: strum 3
+-- 4: strum 4
 include 'lib/nest_/core'
 include 'lib/nest_/norns'
 include 'lib/nest_/grid'
@@ -24,7 +24,7 @@ function play(deg, oct)
 end
 
 strum = nest_ {
-    a = _grid.number {
+    _grid.number {
         x = { 1, 5 },
         y = 1,
         value = math.random(5),
@@ -34,54 +34,54 @@ strum = nest_ {
             play(value, self.y)
         end
     },
-    b = _grid.number {
+    _grid.number {
         x = { 1, 5 },
         y = 2,
         value = math.random(5),
         
         action = function(self, value)
             play(value, self.y)
+            local above = strum[self.key - 1]
             
-            -- decriment & wrap a
             clock.run(function()
                 clock.sleep(0.2)
                 
-                strum.a.value = (strum.a.value == 1) and 5 or (strum.a.value - 1) 
-                strum.a:update()
+                above.value = (above.value == 1) and 5 or (above.value - 1) 
+                above:update()
             end)
         end
     },
-    c = _grid.number {
+    _grid.number {
         x = { 1, 5 },
         y = 3,
         value = math.random(5),
         
         action = function(self, value)
             play(value, self.y)
+            local above = strum[self.key - 1]
             
-            -- randomize b
             clock.run(function()
                 clock.sleep(0.15)
                 
-                strum.b.value = math.random(5)
-                strum.b:update()
+                above.value = math.random(5)
+                above:update()
             end)
         end
     },
-    d = _grid.number {
+    _grid.number {
         x = { 1, 5 },
         y = 4,
         value = math.random(5),
         
         action = function(self, value)
             play(value, self.y)
+            local above = strum[self.key - 1]
             
-            -- incriment & wrap c
             clock.run(function()
                 clock.sleep(0.1)
                 
-                strum.c.value = strum.c.value % 5 + 1,
-                strum.c:update()
+                above.value = above.value % 5 + 1,
+                above:update()
             end)
         end
     }
